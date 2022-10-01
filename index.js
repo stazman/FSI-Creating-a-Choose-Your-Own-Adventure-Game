22a0c0593a17c7cf5a4d87e1ddc0fc79ac40e8a2
Christopher DiStasio <cadistasio@gmail.com>
2022-10-01 12:16:25 -0500

    Adds setup files

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//Your Code here
+// window.prompt('Do you head left or right?')
+
+// let firstAnswer = window.prompt('Do you head left or right?')
+
+// console.log(firstAnswer)
+
+// let firstAnswer  = window.prompt('Do you head left or right?')
+// console.log(firstAnswer === 'left')
+
+let firstAnswer  = window.prompt('Do you head left or right?')
+if(firstAnswer === 'left'){
+    let secondAnswer = window.prompt(`You come across a stray cat.
+It scampers off down a small hole, just large enough for you to crawl through.
+Do you follow it, or continue on your path?`)
+
+} else if(firstAnswer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon.
+On the other side of him, you see a shiny chest of treasure. Another path would
+lead you away from the dragon altogether. Which path do you take?`)
+
+}
